b803d3b784213971ed052e47826d32807bf2bf22
OsamaNagi <alshahidyosama@gmail.com>
2021-11-18 00:17:48 +0200

    responsive footer almost done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 module.exports = {
-	mode: 'jit',
-	purge: ['./public/**/*.html'],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {
-			colors: {
-                darkGray: "#1A1A1A",
-                wineRed: "#A61034",
-                textBlue: "#1034A6",
-			},
-             fontFamily: {
-                nunito: "'Nunito Sans', sans-serif",
-            },
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+  mode: "jit",
+  purge: ["./public/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        darkGray: "#1A1A1A",
+        wineRed: "#A61034",
+        textBlue: "#1034A6",
+        bluredWhite: "#ECF0FD",
+      },
+      fontFamily: {
+        nunito: "'Nunito Sans', sans-serif",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
